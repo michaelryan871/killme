fd9bbedd3b3a0428427ee833aef8b9fa42bed14a
Michael Ryan <mike.ryan871@gmail.com>
2020-03-08 21:40:52 -0400

    Added Borders.

---
+++ Dictionary/src/Frame.java
@@ -0,0 +1,124 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border; 
+
+public class Frame {
+	public static void createAndShowGUI(){
+	      // Create(s) the window
+	      JFrame frame = new JFrame("Dictionary");
+	      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	      // Display(s) the window
+	      frame.setPreferredSize(new Dimension(900, 750));
+	      frame.pack();
+	      frame.setLocationRelativeTo(null);
+
+	      // .setBounds = (int x, int y, int width, int height)
+
+	      // Add(s) "Add" Button
+	      JButton add = new JButton("Add");
+	      add.addActionListener(new ActionListener()
+	      {
+	        public void actionPerformed(ActionEvent e)
+	        {
+	          System.out.println("Add Placeholder");
+	        }
+	      });
+
+	      add.setBounds(5, 5, 100, 35);
+	      frame.add(add);
+
+	      // Add(s) "Remove" Button
+	      JButton remove = new JButton("Remove");
+	      remove.addActionListener(new ActionListener()
+	      {
+	        public void actionPerformed(ActionEvent e)
+	        {
+	          System.out.println("Remove Placeholder");
+	        }
+	      });
+
+	      remove.setBounds(110, 5, 100, 35);
+	      frame.add(remove);
+
+	      // Add(s) "SearchBar" TextField
+
+	      JTextField searchBar = new JTextField("");
+	      searchBar.setBounds(5, 45, 205, 35);
+	      frame.add(searchBar);
+
+	      // Add(s) "Ascending" CheckBox
+	      JCheckBox ascend = new JCheckBox("Asc");
+	      ascend.addActionListener(new ActionListener()
+	      {
+	        public void actionPerformed(ActionEvent e)
+	        {
+	          System.out.println("Ascend Placeholder");
+	        }
+	      });
+
+	      ascend.setBounds(22, 80, 50, 30 );
+	      frame.add(ascend);
+
+	      // Add(s) "Descending" JCheckBox
+	      JCheckBox descend = new JCheckBox("Desc");
+	      ascend.addActionListener(new ActionListener()
+	      {
+	        public void actionPerformed(ActionEvent e)
+	        {
+	          System.out.println("Descend Placeholder");
+	        }
+	      });
+
+	      descend.setBounds(129, 80, 55, 30);
+	      frame.add(descend);
+
+	      // CheckBoxes --> Group
+	      ButtonGroup group = new ButtonGroup();
+	      group.add(ascend);
+	      group.add(descend);
+
+	      // JTextArea --> Stores definitions, etc...
+	      JTextArea definition = new JTextArea();
+	      definition.setBounds(218, 5, 660, 700);
+	      definition.setEditable(false);
+	      frame.add(definition);
+
+	      // List --> Stores words
+	      JTextArea words = new JTextArea(); 
+	      words.setBounds(5,115, 207, 590);
+	      words.setEditable(false);
+	      frame.add(words);
+	      
+	       JScrollPane scroll = new JScrollPane(words);
+	       scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+	      
+	      
+	      // Border(s)
+	      Border wordsBorder = BorderFactory.createLineBorder(Color.BLACK);
+	      words.setBorder(BorderFactory.createCompoundBorder(wordsBorder, BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+	     
+	      Border definitionBorder = BorderFactory.createLineBorder(Color.BLACK);
+	      definition.setBorder(BorderFactory.createCompoundBorder(definitionBorder, BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+	      
+	      Border searchBarBorder = BorderFactory.createLineBorder(Color.BLACK);
+	      searchBar.setBorder(BorderFactory.createCompoundBorder(searchBarBorder, BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+	             
+	      // JFrame Option(s)
+	      frame.setLayout(null);
+	      frame.setVisible(true);
+	      frame.setResizable(false);
+	    }
+
+	    public static void main(String[] args){
+
+	      javax.swing.SwingUtilities.invokeLater(new Runnable(){ // Schedule a job
+	        // for the event-dispatching thread: <-- Received from Oracle's website.
+	        public void run(){
+	          createAndShowGUI();
+	        }
+	      });
+	    }	
+}
